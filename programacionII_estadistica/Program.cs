8887d1d4648de5f266fce2de2790df2e1a1134ba
usis043719 <gdadvid.dubon@gmail.com>
2020-04-30 02:25:05 -0600

    prueba_0

--- programacionII_estadistica/Program.cs
+++ programacionII_estadistica/Program.cs
@@ -16,7 +16,8 @@ namespace programacionII_estadistica
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new clientes());
         }
     }
 }
+//hola
